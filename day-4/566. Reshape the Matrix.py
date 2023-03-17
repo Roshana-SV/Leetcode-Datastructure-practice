29131c56782a5f30a16859150cd21e6c6353a148
Roshana S V <104217663+Roshana-SV@users.noreply.github.com>
2023-03-17 22:47:11 +0530

    Create 566. Reshape the Matrix.py

---
+++ day-4/566. Reshape the Matrix.py
@@ -0,0 +1,11 @@
+import numpy as np
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        """
+        :type mat: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        try: return np.reshape(mat, (r, c))
+        except: return mat
